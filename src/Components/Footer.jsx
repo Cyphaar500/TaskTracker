db5358b7f8cb5d3564dd2a616c41878e606fb100
Cyphaar <victorokafor500@yahoo.com>
2022-10-13 23:43:46 +0100

    changed 2021 to 2022

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="Footer">
-            <h4 className="f-title">Copyright &copy; 2021</h4>
+            <h4 className="f-title">Copyright &copy; 2022</h4>
             <a href="google.com">About</a>
         </div>
     )
